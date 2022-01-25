cb0fb8abd5ca297b7c6f3a7e0ec73d91b4f2db8a
John Gallagher <gallagherj0913@gmail.com>
2022-01-24 17:43:16 -0700

    change to put route in database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put(1, content);
+  const request = store.put(content, 1);
   const result = await request;
   console.log('result.value', result);
   if(!result) {
